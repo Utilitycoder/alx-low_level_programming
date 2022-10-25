b5d5d20e366963c0cc2af8fc28ee22a7f9872252
--replace-all <utility4all@gmail.com>
2022-10-25 10:03:21 -0700

    task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,11 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+
+#endif
